ce092d6c5214ce5c43bfb7f35ee2f8a49b320da4
haideralrustem <haideralrustem@yahoo.com>
2023-04-10 11:51:03 -0500

    tailwind changes

---
+++ src/components/ListingComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SummaryCardComponent from './SummaryCardComponent'
+
+function ListingComponent() {
+  return (
+    <div>
+        
+        <SummaryCardComponent></SummaryCardComponent>
+    </div>
+  )
+}
+
+export default ListingComponent
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeInroComponent from '../components/HomeInroComponent'
+import ListingCopmonent from '../components/ListingComponent'
 
 function HomePage() {
   return (
     <div>
         <HomeInroComponent></HomeInroComponent>
+        <ListingCopmonent></ListingCopmonent>
     </div>
   )
 }
