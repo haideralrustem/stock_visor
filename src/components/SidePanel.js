d5f27c7b1f73d42c0de7e899be92c810b21b8da2
haider-a-alrustem <haider.a.alrustem@uth.tmc.edu>
2023-08-27 11:50:46 -0500

    side-panel

---
+++ src/components/SidePanel.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+
+
+
+export default function SidePanel() {
+  return (
+    <div>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      
+
+    </div>
+  )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeInroComponent from '../components/HomeInroComponent'
 import ListingCopmonent from '../components/ListingComponent'
+import SidePanel from '../components/SidePanel'
 
 function HomePage() {
   return (
     <div className='mx-24'>
         <HomeInroComponent></HomeInroComponent>
         <ListingCopmonent></ListingCopmonent>
+        <SidePanel></SidePanel>
     </div>
   )
 }
